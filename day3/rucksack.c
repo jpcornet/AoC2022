7aa4825eeb5c88dd346e7bb881bbcd542d4b0a7e
Jan-Pieter Cornet <johnpc@xs4all.nl>
2022-12-03 18:18:43 +0100

    part 2.

--- day3/rucksack.c
+++ day3/rucksack.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/* for apple high resolution timer functions*/
+/* for apple high resolution timer functions. Yes, this is apple/Darwin specific. */
 #define HAVE_MACH_TIMER
 #include <mach/mach_time.h>
 
 #define MAXLINE 1000
 
-int process_file(FILE* f) {
-    char linebuf[MAXLINE];
-    int prio_sum = 0;
-    /* variable for high res timer */
+/* return time in nanoseconds */
+static uint64_t ns() {
     static uint64_t is_init = 0;
     static mach_timebase_info_data_t info;
-    uint64_t start, now;
-
     if (0 == is_init) {
-      mach_timebase_info(&info);
-      is_init = 1;
+        mach_timebase_info(&info);
+        is_init = 1;
     }
-    start  = mach_absolute_time();
-    start *= info.numer;
-    start /= info.denom;
+    uint64_t now;
+    now = mach_absolute_time();
+    now *= info.numer;
+    now /= info.denom;
+    return now;
+}
+
+void part1(FILE* f) {
+    char linebuf[MAXLINE];
+    int prio_sum = 0;
+    /* variables for high res timer */
+    uint64_t start, end;
+
+    start = ns();
 
     while ( fgets(linebuf, sizeof(linebuf), f) != NULL ) {
         uint64_t vec = 0; /* 64 bit to keep track of characters */
@@ -76,12 +83,65 @@ int process_file(FILE* f) {
         }
         prio_sum += prio;
     }
-    now  = mach_absolute_time();
-    now *= info.numer;
-    now /= info.denom;
+    end  = ns();
     printf("part 1, total prio: %d\n", prio_sum);
-    printf("part 1 took: %llu ns\n", now - start);
-    return 0;
+    printf("part 1 took: %llu ns\n", end - start);
+}
+
+/* a constant with 54 bits set, to initialize "what is common" mask. */
+#define BITS54 0xFFFFFFFFFFFFFF
+
+void part2(FILE* f) {
+    char linebuf[MAXLINE];
+    int prio_sum = 0;
+    int lineno = 0; /* need to count lines to get groups of 3 */
+    /* groupshared contains all items shared between the group, initialiased as "all present" */
+    uint64_t groupshared = (uint64_t) BITS54;
+
+    /* variables for high res timer */
+    uint64_t start, end;
+
+    start = ns();
+
+    while ( fgets(linebuf, sizeof(linebuf), f) != NULL ) {
+        uint64_t vec = 0;
+        lineno++;
+
+        /* all characters, process until EOL */
+        for ( int i = 0; linebuf[i] != '\n'; i++ ) {
+            /* if character is uppercase, init prio at 26, 0 for lowercase. */
+            char c = linebuf[i];
+            int char_prio = 26 * (c <= 'Z');
+            c |= 0x20; /* make sure c is lower case now */
+            c -= 'a'; /* c is now between 0 and 25 */
+            if ( c < 0 || c > 25 ) {
+                fprintf(stderr, "Invalid character %c in line [%s], abort\n", linebuf[i], linebuf);
+                exit(-1);
+            }
+            char_prio += c + 1;
+            vec |= (uint64_t) 1 << char_prio;
+        }
+        /* calculate which characters are shared between this group */
+        groupshared &= vec;
+
+        /* if we processed the 3rd line, get the group badge: only item all have in common */
+        if ( lineno % 3 == 0 ) {
+            if ( ! groupshared ) {
+                fprintf(stderr, "No chars in common at line %d\n", lineno);
+            } else {
+                int prio = __builtin_ctzll(groupshared);
+                /* sanity check, make sure this prio is only bit set in groupshared */
+                if ( (uint64_t) 1 << prio != groupshared ) {
+                    fprintf(stderr, "Warning, multiple chars in common at line %d, groupshared is %llx\n", lineno, groupshared);
+                }
+                prio_sum += prio;
+            }
+            groupshared = BITS54;
+        }
+    }
+    end  = ns();
+    printf("part 2, total prio: %d\n", prio_sum);
+    printf("part 2 took: %llu ns\n", end - start);
 }
 
 int main(int argc, char** argv) {
@@ -94,5 +154,8 @@ int main(int argc, char** argv) {
         perror("Error opening input file");
         exit(2);
     }
-    return process_file(f);
+    part1(f);
+    fseek(f, 0, SEEK_SET);
+    part2(f);
+    return 0;
 }
