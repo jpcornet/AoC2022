4ebd394404b6a6a0f77aa1c9dbd5f388192cfbb7
Jan-Pieter Cornet <johnpc@xs4all.nl>
2022-12-04 13:07:39 +0100

    do part 2 in parallel.

--- day4/cleanup.sh
+++ day4/cleanup.sh
@@ -10,8 +10,10 @@ error() {
 
 in="$1"
 
-part1() {
+part12() {
     local contained=0
+    local overlaps=0
+    local r1start r1end r2start r2end
     while read line; do
         # split ranges into start and end
         IFS="$IFS-" read r1start r1end <<<${line%,*}
@@ -24,12 +26,19 @@ part1() {
         if [ $r1start -ge $r2start -a $r1start -le $r2end -a $r1end -ge $r2start -a $r1end -le $r2end ]; then
             echo XXX range 1 $r1start-$r1end is contained in range 2 $r2start-$r2end
             contained=$(($contained + 1))
+            overlaps=$(($overlaps + 1))
         elif [ $r2start -ge $r1start -a $r2start -le $r1end -a $r2end -ge $r1start -a $r2end -le $r1end ]; then
             echo XXX range 1 $r1start-$r1end contains range 2 $r2start-$r2end
             contained=$(($contained + 1))
+            overlaps=$(($overlaps + 1))
+        # there is overlap if start or end of a range is within the other range
+        elif [ \( $r1start -ge $r2start -a $r1start -le $r2end \) -o \( $r1end -ge $r2start -a $r1end -le $r2end \) ]; then
+            echo XXX overlap between ranges: $r1start-$r1end and $r2start-$r2end
+            overlaps=$(($overlaps + 1))
         fi
     done < "$1"
     echo part 1, total pairs where one contains the other: $contained
+    echo part 2, total pairs where there is overlap: $overlaps
 }
 
-part1 "$in"
+part12 "$in"
