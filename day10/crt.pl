ad03c00482e5478b22efba2248d4599c4962f2bf
Jan-Pieter Cornet <johnpc@xs4all.nl>
2022-12-10 11:27:43 +0100

    now also part 2

--- day10/crt.pl
+++ day10/crt.pl
@@ -5,20 +5,33 @@ use warnings;
 
 my $x = 1;
 my $cycle = 0;
+# ss is the total "signal strength" at cycle 20, 60, ... for part 1
 my $ss = 0;
+# at what moment we should do part 1 signal strength
 my $at = 20;
+# collect CRT lines here (as one line)
+my $crt = '';
 
 while ( <> ) {
     my ($op, $val) = split;
     my $newx = $x;
+    # number of cycles this instruction takes
+    my $takes;
     if ( $op eq 'noop' ) {
-        $cycle += 1;
+        $takes = 1;
     } elsif ( $op eq 'addx' ) {
-        $cycle += 2;
+        $takes = 2;
         $newx = $x + $val;
     } else {
         die "Invalid op $op\n";
     }
+    for ( 1 .. $takes ) {
+        # if the sprite is at the CRT position ( = cycle num ) then we draw a lit pixel
+        my $pixel = abs($x - $cycle % 40) <= 1 ? '#' : '.';
+        $crt .= $pixel;
+        $cycle++;
+    }
+    # part 1 signal strength
     if ( $cycle >= $at ) {
         chomp;
         $ss += $at * $x;
@@ -27,3 +40,6 @@ while ( <> ) {
     }
     $x = $newx;
 }
+
+print "Part 2, CRT result:\n";
+print substr($crt, $_ * 40, 40), "\n" for 0 .. 5;
