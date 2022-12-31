a347cdbd602313aa7ad4536a8250ef7975c94237
Jan-Pieter Cornet <johnpc@xs4all.nl>
2022-12-31 02:33:28 +0100

    work in progress on day19

--- day19/geodes.go
+++ day19/geodes.go
@@ -17,10 +17,9 @@ type Blueprint struct {
 	obs_clay_cost  int
 	geode_ore_cost int
 	geode_obs_cost int
-	max_ore_robot  int
+	max_ore_robot  int // not sure if this is needed
 }
 
-// XXX recode... state should only be amount of material at start of minute.
 type State struct {
 	timeleft    int
 	ore         int
@@ -68,41 +67,10 @@ func parse_input(filename string) []Blueprint {
 	return results
 }
 
+// Path is a collection of states. Current state is the final one.
 type Path struct {
-	bp     *Blueprint
 	states []State
 	score  int
-}
-
-type Solutions []Path
-
-// Implement the interface needed to sort Solutions based on score. Note that it sorts highest score first
-//func (s Solutions) Len() int { return len(s) }
-//func (s Solutions) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
-//func (s Solutions) Less(i, j int) bool { return s[j].Score() < s[i].Score() }
-
-func fill_solution(bp Blueprint, state State) Path {
-	var path Path
-	path.bp = &bp
-	path.states = make([]State, 0, state.timeleft)
-	elem := state
-	for elem.timeleft > 0 {
-		elem.ore += elem.ore_robot
-		elem.clay += elem.clay_robot
-		elem.obsidian += elem.obs_robot
-		elem.geode += elem.geode_robot
-		path.states = append(path.states, elem)
-		elem.timeleft--
-	}
-	return path
-}
-
-func (p Path) Copy() Path {
-	var new Path
-	new.bp = p.bp
-	new.states = make([]State, len(p.states))
-	copy(new.states, p.states)
-	return new
 }
 
 // return the minimum of 2 integers
@@ -114,212 +82,156 @@ func intmin(x, y int) int {
 	}
 }
 
-func (p Path) Score() int {
+func (p Path) Score(bp Blueprint) int {
 	if p.score != 0 {
 		return p.score
 	}
-	final := p.states[len(p.states)-1]
+	s := p.states[len(p.states)-1]
 	// geodes are obviously best, score them good.
-	p.score = final.geode * 1_000_000_000
+	p.score = s.geode * 1_000_000_000
 	// geode cracking robots are second best, score according to how many we could have built
-	p.score += intmin(final.ore*1_000_000/p.bp.geode_ore_cost, final.obsidian*1_000_000/p.bp.geode_obs_cost)
+	p.score += intmin(s.ore*1_000_000/bp.geode_ore_cost, s.obsidian*1_000_000/bp.geode_obs_cost)
 	// obsidian collecting robots are third, again score to how many could have been built
-	p.score += intmin(final.ore*1000/p.bp.obs_ore_cost, final.clay*1000/p.bp.obs_clay_cost)
+	p.score += intmin(s.ore*1000/bp.obs_ore_cost, s.clay*1000/bp.obs_clay_cost)
 	// clay collecting robots are next, scoring even less
-	p.score += final.ore / p.bp.clay_cost
+	p.score += s.ore / bp.clay_cost
 	return p.score
 }
 
-// Find the first spot where there is enough ore, clay and obsidian as specified in the minstate
-// note that other state elements aren't used
-func (p Path) FindMinState(startpos int, minstate State) int {
-	for i, state := range p.states[startpos : len(p.states)-1] {
-		if state.ore >= minstate.ore && state.clay >= minstate.clay && state.obsidian >= minstate.obsidian {
-			// we might've already added a robot at this point, so make sure the materials are still available at the next step
-			nextstate := p.states[i+1]
-			if nextstate.ore >= minstate.ore && nextstate.clay >= minstate.clay && nextstate.obsidian >= minstate.obsidian {
-				// return next state, because elements only become available at the end of the minute.
-				return startpos + i + 1
-			}
-		}
-	}
-	return -1
+// an original state and the extra robots we will build here
+type BuildState struct {
+	s                                             State
+	ore_robot, clay_robot, obs_robot, geode_robot int
 }
 
-// Add a robot to the solution. Returns true on success
-// return false on failure (when there is not enough material), and note that state of p will be inconsistent
-func (p Path) AddRobot(pos int, rstate State) bool {
-	for i := pos; i < len(p.states); i++ {
-		if rstate.ore_robot > 0 {
-			p.states[i].ore -= p.bp.ore_cost * rstate.ore_robot
-			// number of available materials cannot drop below number of robots delivering this
-			if p.states[i].ore < p.states[i].ore_robot {
-				return false
+func possible_next_steps(p Path, bp Blueprint) []Path {
+	state := p.states[len(p.states)-1]
+	// collect possible next steps, as BuildStates
+	var next_step []BuildState
+	// try adding all robot types, as much as possible
+	// start with ore robots. Include adding 0 robots
+	// XXX no ore robots for now
+	//for ore_robot := 0; state.ore >= ore_robot*bp.ore_cost; ore_robot++ {
+	for ore_robot := 0; ore_robot < 1; ore_robot++ {
+		new_state := BuildState{s: state, ore_robot: ore_robot}
+		new_state.s.ore -= ore_robot * bp.ore_cost
+		next_step = append(next_step, new_state)
+	}
+	// try adding clay robots, at least one. Add to more_next_step.
+	var clay_next_step []BuildState
+	for clay_robot := 1; state.ore >= clay_robot*bp.clay_cost; clay_robot++ {
+		// try adding to all the new states, stop if there are not enough materials
+		for _, ns := range next_step {
+			if ns.s.ore < clay_robot*bp.clay_cost {
+				break
 			}
-			p.states[i].ore_robot += rstate.ore_robot
-			// each robot produces 0 the minute it is built, but 1 each minute afterwards
-			p.states[i].ore += rstate.ore_robot * (i - pos)
-		}
-		if rstate.clay_robot > 0 {
-			p.states[i].ore -= p.bp.clay_cost * rstate.clay_robot
-			if p.states[i].ore < p.states[i].ore_robot {
-				return false
-			}
-			p.states[i].clay_robot += rstate.clay_robot
-			p.states[i].clay += rstate.clay_robot * (i - pos)
-		}
-		if rstate.obs_robot > 0 {
-			p.states[i].ore -= p.bp.obs_ore_cost * rstate.obs_robot
-			p.states[i].clay -= p.bp.obs_clay_cost * rstate.obs_robot
-			if p.states[i].ore < p.states[i].ore_robot || p.states[i].clay < p.states[i].clay_robot {
-				return false
-			}
-			p.states[i].obs_robot += rstate.obs_robot
-			p.states[i].obsidian += rstate.obs_robot * (i - pos)
-		}
-		if rstate.geode_robot > 0 {
-			p.states[i].ore -= p.bp.geode_ore_cost * rstate.geode_robot
-			p.states[i].obsidian -= p.bp.geode_obs_cost * rstate.geode_robot
-			if p.states[i].ore < p.states[i].ore_robot || p.states[i].obsidian < p.states[i].obs_robot {
-				return false
-			}
-			p.states[i].geode_robot += rstate.geode_robot
-			p.states[i].geode += rstate.geode_robot * (i - pos)
+			new_state := BuildState{s: ns.s, clay_robot: clay_robot}
+			new_state.s.ore -= clay_robot * bp.clay_cost
+			clay_next_step = append(clay_next_step, new_state)
 		}
 	}
-	return true
-}
+	next_step = append(next_step, clay_next_step...)
+	// try adding obsidian robots, at least one
+	var obs_next_step []BuildState
+	for obs_robot := 1; state.ore >= obs_robot*bp.obs_ore_cost && state.clay >= obs_robot*bp.obs_clay_cost; obs_robot++ {
+		for _, ns := range next_step {
+			if ns.s.ore < obs_robot*bp.obs_ore_cost || ns.s.clay < obs_robot*bp.obs_clay_cost {
+				continue
+			}
+			new_state := BuildState{s: ns.s, obs_robot: obs_robot}
+			new_state.s.ore -= obs_robot * bp.obs_ore_cost
+			new_state.s.clay -= obs_robot * bp.obs_clay_cost
+			obs_next_step = append(obs_next_step, new_state)
+		}
+	}
+	next_step = append(next_step, obs_next_step...)
+	// try adding geode cracking robots, at least one
+	var geo_next_step []BuildState
+	for geode_robot := 1; state.ore >= geode_robot*bp.geode_ore_cost && state.obsidian >= geode_robot*bp.geode_obs_cost; geode_robot++ {
+		for _, ns := range next_step {
+			if ns.s.ore < geode_robot*bp.geode_ore_cost || ns.s.obsidian < geode_robot*bp.geode_obs_cost {
+				continue
+			}
+			new_state := BuildState{s: ns.s, geode_robot: geode_robot}
+			new_state.s.ore -= geode_robot * bp.geode_ore_cost
+			new_state.s.obsidian -= geode_robot * bp.geode_obs_cost
+			geo_next_step = append(geo_next_step, new_state)
+		}
+	}
+	next_step = append(next_step, geo_next_step...)
 
-func improve(c Path) Solutions {
-	// collect new solutions here
-	new_solutions := make(Solutions, 0, 4)
-	// try adding geode-cracking robots, if possible. Start searching at the beginning
-	startpos := 0
-	// loop until we inserted one geode-cracking robot
-	for {
-		insert_geode_robot := c.FindMinState(startpos, State{ore: c.bp.geode_ore_cost, obsidian: c.bp.geode_obs_cost})
-		if insert_geode_robot < 0 {
-			break
-		}
-		solution := c.Copy()
-		if solution.AddRobot(insert_geode_robot, State{geode_robot: 1}) {
-			new_solutions = append(new_solutions, solution)
-			break
-		}
-		startpos = insert_geode_robot
+	// now we actually go and build the updated paths
+	next_path := make([]Path, len(next_step))
+	for i, ns := range next_step {
+		next_path[i].states = make([]State, len(p.states)+1)
+		copy(next_path[i].states, p.states)
+		// advance the state by one minute
+		ns.s.timeleft--
+		// materials are being delivered by the robots
+		ns.s.ore += ns.s.ore_robot
+		ns.s.clay += ns.s.clay_robot
+		ns.s.obsidian += ns.s.obs_robot
+		ns.s.geode += ns.s.geode_robot
+		// and the new robots are delivered
+		ns.s.ore_robot += ns.ore_robot
+		ns.s.clay_robot += ns.clay_robot
+		ns.s.obs_robot += ns.obs_robot
+		ns.s.geode_robot += ns.geode_robot
+		// store this new state at the end of the new path
+		next_path[i].states[len(p.states)] = ns.s
 	}
-	// get the final state to check against amount of materials we have at the end
-	final := c.states[len(c.states)-1]
-	// if we need more obsidian, try inserting an obsidan-collecting robot
-	// we only need obsidian if the ratio between ore and obsidian is below what is needed to build the robot
-	need_obsidian := false
-	if final.obsidian <= final.ore*c.bp.geode_obs_cost/c.bp.geode_ore_cost {
-		need_obsidian = true
-		startpos = 0
-		for {
-			insert_obs_robot := c.FindMinState(startpos, State{ore: c.bp.obs_ore_cost, clay: c.bp.obs_clay_cost})
-			if insert_obs_robot < 0 {
-				break
-			}
-			solution := c.Copy()
-			if solution.AddRobot(insert_obs_robot, State{obs_robot: 1}) {
-				new_solutions = append(new_solutions, solution)
-				break
-			}
-			startpos = insert_obs_robot
-		}
-	}
-	// if we need more clay, try inserting a clay-collecting robot
-	// we only need more clay if we need more obsidian and the ratio of clay and ore, is less than what is needed for an obsidian robot
-	if need_obsidian && final.clay <= final.ore*c.bp.obs_clay_cost/c.bp.obs_ore_cost {
-		startpos = 0
-		for {
-			insert_clay_robot := c.FindMinState(startpos, State{ore: c.bp.clay_cost})
-			if insert_clay_robot < 0 {
-				break
-			}
-			solution := c.Copy()
-			if solution.AddRobot(insert_clay_robot, State{clay_robot: 1}) {
-				new_solutions = append(new_solutions, solution)
-				break
-			}
-			startpos = insert_clay_robot
-		}
-	}
-	// only try extra ore if allowed
-	if final.ore_robot < c.bp.max_ore_robot {
-		startpos = 0
-		for {
-			insert_ore_robot := c.FindMinState(startpos, State{ore: c.bp.ore_cost})
-			if insert_ore_robot < 0 {
-				break
-			}
-			solution := c.Copy()
-			if solution.AddRobot(insert_ore_robot, State{ore_robot: 1}) {
-				new_solutions = append(new_solutions, solution)
-				break
-			}
-			startpos = insert_ore_robot
-		}
-	}
-	return new_solutions
+	return next_path
 }
 
 func get_max_geodes(bp Blueprint, state State) Path {
-	// keep the best solution so far here
-	best := Path{score: -1}
-	// and keep a list of possible solutions
-	possible := make(Solutions, 0, 10)
-	max_ore_robot := 1
-	for {
-		fmt.Printf("Trying with max ore robots=%d\n", max_ore_robot)
-		bp.max_ore_robot = max_ore_robot
-		possible = append(possible, fill_solution(bp, state))
-		seen := make(map[State]bool)
-		showtime := time.Now()
-		considered := 0
-		for len(possible) > 0 {
-			if time.Since(showtime).Seconds() > 1.0 {
-				fmt.Printf("Considered %d solutions, %d possible solutions in pipeline\n", considered, len(possible))
-				showtime = time.Now()
-			}
-			// take first solution from the list
-			candidate := possible[0]
-			possible = possible[1:]
-			new_solutions := improve(candidate)
-			for _, s := range new_solutions {
-				if _, already_done := seen[s.states[len(s.states)-1]]; already_done {
+	// keep a list of possible states here
+	solutions := make([]Path, 1)
+	solutions[0] = Path{states: []State{state}}
+	seen := make(map[State]bool)
+	seen_hits := 0
+	seen_miss := 0
+	for len(solutions) > 0 {
+		fmt.Printf("timeleft=%d, considering %d possible solutions\n", solutions[0].states[len(solutions[0].states)-1].timeleft, len(solutions))
+		new_solutions := make([]Path, 0, len(solutions))
+		for _, s := range solutions {
+			next_step := possible_next_steps(s, bp)
+			for _, ns := range next_step {
+				// check if we haven't seen this state before, skip if we have
+				// We check the state without the timeleft field.
+				verify_state := ns.states[len(ns.states)-1]
+				verify_state.timeleft = 0
+				if _, already_seen := seen[verify_state]; already_seen {
+					seen_hits++
 					continue
 				} else {
-					seen[s.states[len(s.states)-1]] = true
-					considered++
+					seen[verify_state] = true
+					seen_miss++
 				}
-				if s.Score() > best.Score() {
-					fmt.Printf("New best solution, score=%d: %v\n", s.Score(), s)
-					best = s.Copy()
-				}
-				position, _ := sort.Find(len(possible), func(i int) int {
-					if s.Score() < possible[i].Score() {
+				position, _ := sort.Find(len(new_solutions), func(i int) int {
+					if ns.Score(bp) < new_solutions[i].Score(bp) {
 						return 1
-					} else if s.Score() > possible[i].Score() {
+					} else if ns.Score(bp) > new_solutions[i].Score(bp) {
 						return -1
 					} else {
 						return 0
 					}
 				})
-				possible = append(possible, Path{})
-				copy(possible[position+1:], possible[position:])
-				possible[position] = s
+				new_solutions = append(new_solutions, Path{})
+				copy(new_solutions[position+1:], new_solutions[position:])
+				new_solutions[position] = ns
 			}
 		}
-		if best.states[len(best.states)-1].ore_robot < max_ore_robot {
+		solutions = new_solutions
+		if len(solutions) == 0 {
+			fmt.Printf("No solutions found?\n")
+			return Path{}
+		}
+		if solutions[0].states[len(solutions[0].states)-1].timeleft == 0 {
 			break
-		} else {
-			// try to see if adding 1 ore robot gives a better solution
-			max_ore_robot += 1
 		}
 	}
-	return best
+	// the first one is always the best solution, since they are ordered
+	return solutions[0]
 }
 
 func main() {
